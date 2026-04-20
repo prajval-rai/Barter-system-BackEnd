6af43ceb08504d3fc6367ef560a59c8bd2142a3d
prajval-rai <prajvalrai2001@gmail.com>
2026-04-20 23:26:37 +0530

    web socket issue fix

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('history/<int:request_id>/', views.message_history, name='chat-history'),
     path("request/<int:pk>/otp/generate/", views.generate_otp),
   path("request/<int:pk>/otp/verify/",   views.verify_otp),
+  path("ws-token/",   views.get_ws_token),
 #   path("barter/rate/<int:pk>/",                 views.rate_trade)
 ]
